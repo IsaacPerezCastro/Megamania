b4ec3c79cbca1635f5806b4203ff9aa000370dbe
IsaacPerez <iperez_19@alu.uabcs.mx>
2020-03-26 17:29:50 -0700

    Repositorio creado

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <windows.h>
+#include <stdexcept>
+int main(int argc, char** argv) {
+	cout<<"Holamundo";
+	getch();
+	return 0;
+}
